5cb4a0c9fe6204004c383eb588c2c01925b3c0cb
Charles Covar <ccovar@flexgen.com>
2024-11-07 23:22:47 -0500

    Fixes telescope

--- dot_config/nvim/lua/plugins/telescope.lua
+++ dot_config/nvim/lua/plugins/telescope.lua
@@ -1,18 +1,8 @@
 
-local builtin = require "telescope.builtin"
-
 return {
   {
     'nvim-telescope/telescope.nvim', tag = '0.1.5',
     dependencies = { 'nvim-lua/plenary.nvim' },
-    keys = {
-      { "<leader>ff", builtin.find_files, mode = "n" },
-      { "<leader>fg", builtin.live_grep, mode = "n" },
-      { "<leader>fb", builtin.buffers, mode = "n" },
-      { "<leader>fh", builtin.help_tags, mode = "n" },
-      { "<leader>fr", builtin.oldfiles, mode = "n" },
-      { "<leader>fc", builtin.commands, mode = "n" },
-    },
     opts = {},
   },
 }
