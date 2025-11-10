545510bec6d3602e667c5e29bb76e54d9f72627e
Charles Covar <covar1@gmail.com>
2025-11-10 10:43:55 -0500

    fix: Neovim filetype improvements

--- dot_config/nvim/ftplugin/markdown.lua
+++ dot_config/nvim/ftplugin/markdown.lua
@@ -2,4 +2,6 @@ require("nvim-treesitter").install({ "markdown" })
 vim.treesitter.start()
 vim.wo.foldexpr = "v:lua.vim.treesitter.foldexpr()"
 vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
-vim.o.conceallevel = 2
+vim.w.conceallevel = 2
+vim.wo.wrap = true
+vim.wo.linebreak = true
--- dot_config/nvim/ftplugin/zig.lua
+++ dot_config/nvim/ftplugin/zig.lua
@@ -1,1 +1,17 @@
+require("nvim-treesitter").install({ "zig" }):wait(300000)
+
+vim.treesitter.start()
+vim.wo.foldexpr = "v:lua.vim.treesitter.foldexpr()"
+vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+
 vim.lsp.enable("zls")
+
+vim.api.nvim_create_autocmd("LspAttach", {
+  callback = function()
+    vim.api.nvim_create_autocmd("BufWritePre", {
+      callback = function()
+        vim.lsp.buf.format({ name = "zls", async = false })
+      end,
+    })
+  end,
+})
