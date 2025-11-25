d9b3083aa28249dff5564ad5536b4a19cb8a07bc
Charles Covar <ccovar@flexgen.com>
2025-11-25 09:00:12 -0500

    feat: Treesitter options for caddy filetype

--- dot_config/nvim/ftplugin/caddy.lua
+++ dot_config/nvim/ftplugin/caddy.lua
@@ -1,3 +1,5 @@
 require("nvim-treesitter").install({ "caddy" }):wait(300000)
 
 vim.treesitter.start()
+vim.wo.foldexpr = "v:lua.vim.treesitter.foldexpr()"
+vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
