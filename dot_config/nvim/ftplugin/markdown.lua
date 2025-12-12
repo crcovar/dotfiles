4bb2257eabbe11b76b378fbbf16b425b2ae64701
Charles Covar <ccovar@flexgen.com>
2025-12-12 09:29:57 -0500

    fix: Apply markdown conceallevel to wo

--- dot_config/nvim/ftplugin/markdown.lua
+++ dot_config/nvim/ftplugin/markdown.lua
@@ -2,6 +2,6 @@ require("nvim-treesitter").install({ "markdown" })
 vim.treesitter.start()
 vim.wo.foldexpr = "v:lua.vim.treesitter.foldexpr()"
 vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
-vim.w.conceallevel = 2
+vim.wo.conceallevel = 2
 vim.wo.wrap = true
 vim.wo.linebreak = true
