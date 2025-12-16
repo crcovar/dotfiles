506ecef018d3b663444bb0ad7b8bc1e1ac9d50a0
Charles Covar <ccovar@flexgen.com>
2025-12-16 16:41:29 -0500

    feat: Disable tokyonight theme

--- dot_config/nvim/lua/plugins/theme.lua
+++ dot_config/nvim/lua/plugins/theme.lua
@@ -23,7 +23,7 @@ return {
     "folke/tokyonight.nvim",
     priority = 1000,
     config = function()
-      vim.cmd.colorscheme("tokyonight-night")
+      -- vim.cmd.colorscheme("tokyonight-night")
     end,
   },
   -- Bottom status bar
