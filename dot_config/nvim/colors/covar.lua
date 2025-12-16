6b3a343de23b386cfb0fc06f962efc6fd95569ae
Charles Covar <ccovar@flexgen.com>
2025-12-16 10:02:26 -0500

    feat: Custom colorscheme for ghostty

--- dot_config/nvim/colors/covar.lua
+++ dot_config/nvim/colors/covar.lua
@@ -1,2 +1,24 @@
-vim.api.nvim_set_hl(0, "LineNrAbove", { fg = "#26854c", bg = "black", bold = true })
-vim.api.nvim_set_hl(0, "LineNrBelow", { fg = "#26854c", bg = "black", bold = true })
+local palette = {
+  black = "#000000",
+  red = "#ac2847",
+  green = "#26854c",
+  yellow = "#e98537",
+  blue = "#3859b3",
+  magenta = "#9a4d76",
+  cyan = "#36c5f4",
+  white = "#b0a7b8",
+
+  bright = {
+    black = "#10121c",
+    red = "#ec273f",
+    green = "#5ab552",
+    yellow = "#f3a833",
+    blue = "#3388de",
+    magenta = "#c878af",
+    cyan = "#6dead6",
+    white = "#ffffff",
+  },
+}
+
+vim.api.nvim_set_hl(0, "LineNrAbove", { fg = palette.green, bg = palette.black, bold = true })
+vim.api.nvim_set_hl(0, "LineNrBelow", { fg = palette.green, bg = palette.black, bold = true })
